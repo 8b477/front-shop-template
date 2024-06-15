77a974c7ab2237ccaeb356ddbb6860e257c788e1
8b477 <jonathan_buchet@outlook.fr>
2024-06-15 17:44:38 +0200

    :bricks: added the shop page and adjusted the height of the navigation bar :bricks:

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ArticleComponent } from '../../components/article/article.component';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [ArticleComponent],
+  imports: [ArticleComponent, FormsModule],
   templateUrl: './shop.component.html',
 })
-export class ShopComponent {}
+export class ShopComponent {
+  searchItem: string = '';
+
+  selectOptions: string[] = [
+    'Fruit',
+    'Boisson non alcoolisé',
+    'Boisson alcoolisé',
+    'Surgeler',
+    'Bio',
+    'Conserve',
+  ];
+}
