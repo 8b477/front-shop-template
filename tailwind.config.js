ffd14b8684ff06c9dc9aa0a6cb7159529d6cf5d1
8b477 <jonathan_buchet@outlook.fr>
2024-08-06 23:00:49 +0200

    Add new setup color and improve display

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#1E2334",
+        subtitle: "#956DFF",
+        secondary: "#383E99",
+        textPrimary: "#DCD9F7",
+        textSecondary: "#FFF3D7",
+        button: "#756FFF",
+        card: "#2C3049",
+      },
+    },
   },
   plugins: [],
-}
-
+};
